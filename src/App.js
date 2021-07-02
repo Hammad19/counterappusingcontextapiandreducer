0d86311762f1077fff651ac74c6e28ec8e17be6f
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2021-07-02 19:50:35 +0500

    created countercontext.js parent.js and child.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Parent from './parent.js'
+import counterContext from './countercontext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <counterContext.Provider value = {25}>
+      <div>
+        <Parent/>
+      </div>
+   </counterContext.Provider>
   );
 }
 

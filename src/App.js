3b5a163f299b51bf13fcce6b67367eb79132f19e
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2021-07-02 20:21:07 +0500

    applied React hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Parent from './parent.js'
 import counterContext from './countercontext';
 
 function App() {
+  let countState = React.useState(1);
   return (
-    <counterContext.Provider value = {25}>
+    <counterContext.Provider value = {countState}>
       <div>
         <Parent/>
       </div>
--- src/child.js
+++ src/child.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import counterContext from './countercontext';
 
 const Child = () => {
+    let counterValue = React.useContext(counterContext);
     return(
-        <div>
+        <div className = 'App'>
+            <h1>Counter Value is: {counterValue[0]}</h1>
+            <button onClick ={()=> {counterValue[1](++counterValue[0])}}>Increament</button>
+            
 
         </div>)
 }
